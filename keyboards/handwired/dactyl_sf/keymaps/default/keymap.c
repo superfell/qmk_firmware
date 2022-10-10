4bf3644d435d36e377f6cc8af7a4b38e58ccd599
Simon Fell <fellforce@gmail.com>
2022-10-10 09:51:33 -0700

    dactyl keymap update

--- keyboards/handwired/dactyl_sf/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_sf/keymaps/default/keymap.c
@@ -23,30 +23,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q  ,KC_W   , KC_F  , KC_P  , KC_B  ,                                        KC_J, KC_L, KC_U,    KC_Y,  KC_SEMICOLON, KC_EQUAL,
         KC_GRAVE, KC_A  ,KC_R   , KC_S  , KC_T  , KC_G  ,                                        KC_M, KC_N, KC_E,    KC_I,  KC_O,         KC_QUOTE,
         KC_LSFT,  KC_Z , KC_X   , KC_C  , KC_D  , KC_V  ,                                        KC_K, KC_H, KC_COMMA,KC_DOT,KC_SLASH,     KC_MINUS,
-                        RAISE   , LOWER  ,                                                                         RAISE  , LOWER  ,
-                                                KC_BSPC,KC_LEFT_GUI,KC_LEFT_SHIFT ,        KC_RIGHT_SHIFT , KC_ENTER  ,KC_SPACE
+                        RAISE   , LOWER,                                                                     LOWER,   RAISE,
+                                                KC_SPACE, KC_LEFT_GUI, KC_LEFT_SHIFT,  KC_RIGHT_SHIFT, KC_ENTER, KC_BSPC
     )
-    // ,b
-    // [_LOWER] = LAYOUT_6x6(
-    //     _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
-    //     _______,KC_EXLM,KC_AT  ,KC_LBRC,KC_RBRC,KC_PIPE,                                       KC_UP  ,KC_P7  ,KC_P8  ,KC_P9  ,KC_PAST,KC_F12 ,
-    //     _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_GRV ,                                       KC_DOWN,KC_P4  ,KC_P5  ,KC_P6  ,KC_PPLS,_______,
-    //     _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,                                       KC_AMPR,KC_P1  ,KC_P2  ,KC_P3  ,KC_PSLS,_______,
-    //     QK_BOOT  ,_______,_______,_______,_______,_______,                                       _______,_______,KC_PDOT,KC_P0  ,KC_PEQL,_______,
-    //                                             _______,_______,_______,       _______,_______,_______,
-    //                                                             _______,       _______,
-    //                                                             _______,       _______
-    // ),
-    // [_RAISE] = LAYOUT_6x6(
-    //     _______,_______,_______,_______,_______,_______,                                       _______,_______,_______,_______,_______,_______,
-    //     _______,_______,_______,KC_MS_U,_______,_______,                                       _______,_______,_______,_______,_______,_______,
-    //     _______,_______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                                       _______,_______,_______,_______,_______,KC_MPLY,
-    //     _______,_______,_______,_______,_______,_______,                                       _______,_______,KC_MPRV,KC_MNXT,_______,_______,
-    //     QK_BOOT  ,_______,_______,KC_BTN1,KC_BTN2,_______,                                       KC_WBAK,KC_VOLU,KC_VOLD,KC_MUTE,_______,_______,
-    //                                             _______,_______,_______,       _______,_______,_______,
-    //                                                             _______,       _______,
-    //                                                             _______,       _______
-    // )
+    ,
+    [_LOWER] = LAYOUT_6x6(
+        _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
+        _______,KC_EXLM,KC_AT  ,KC_LBRC,KC_RBRC,KC_PIPE,                                       _______,_______,KC_UP  ,_______,KC_PAST,KC_F12 ,
+        _______,KC_HASH,KC_DLR ,KC_LPRN,KC_RPRN,KC_GRV ,                                       _______,KC_LEFT,KC_DOWN,KC_RIGHT,KC_PPLS,_______,
+        _______,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,                                       KC_AMPR,_______,_______,_______,KC_PSLS,_______,
+                         _______,_______,                                                                      _______,_______,
+                                                _______,_______,KC_LEFT_CTRL,       KC_RIGHT_ALT,_______,_______
+    ),
+    [_RAISE] = LAYOUT_6x6(
+        _______,_______,_______,_______,_______,_______,                                       _______,_______,_______,_______,_______,_______,
+        _______,_______,_______,KC_MS_U,_______,_______,                                       _______,_______,_______,_______,_______,_______,
+        _______,_______,KC_MS_L,KC_MS_D,KC_MS_R,_______,                                       _______,KC_VOLU,KC_VOLD,KC_MUTE,_______,KC_MPLY,
+        QK_BOOT,_______,_______,KC_BTN1,KC_BTN2,_______,                                       KC_WBAK,_______,KC_MPRV,KC_MNXT,_______,_______,
+                         _______,_______,                                                                      _______,_______,
+                                                _______,_______,_______,       _______,_______,_______
+    )
 };
 
 
